a73d6cbde0a1839bbc5855ee9838524da0244f86
Afanasev Mikhail <miafanasev@gmail.com>
2024-06-07 10:16:35 +0700

    Реализована работа с cache

---
+++ src/main/java/ru/innotech/lesson02proxy/Fractionable.java
@@ -0,0 +1,7 @@
+package ru.innotech.lesson02proxy;
+
+public interface Fractionable {
+    double doubleValue();
+    void setNum(int num);
+    void setDenum(int denum1);
+}
--- src/main/java/ru/innotech/lesson02proxy/Starter.java
+++ src/main/java/ru/innotech/lesson02proxy/Starter.java
@@ -2,6 +2,14 @@ package ru.innotech.lesson02proxy;
 
 public class Starter {
     public static void main(String[] args) {
-        System.out.println("Тест");
+        Fraction fraction = new Fraction(7, 2);
+        Fractionable fproxy = Utils.cache(fraction);
+
+        System.out.println(fproxy.doubleValue());
+        System.out.println(fproxy.doubleValue());
+        System.out.println(fproxy.doubleValue());
+        fproxy.setDenum(4);
+        System.out.println(fproxy.doubleValue());
+        System.out.println(fproxy.doubleValue());
     }
 }
